f77d558ff8e169ca8468b30e1a002d3d357e2a69
gurpreet007 <gurpreet007@gmail.com>
2017-10-30 22:54:11 +1030

    test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "He00llo, World!"
+    render html: "He0023llo, World!"
   end
 end
